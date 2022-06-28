0d1c2f1302b379eab76de763d8f0a90b350a59b6
Yajahira <yari@yvelazquez.com>
2022-06-28 17:07:22 -0400

    imported about component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -23,7 +24,7 @@ class App extends React.Component {
             </Route>
             <Route 
               exact path="/about"
-              element={<About />}
+              element={<About/>}
             >
             </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
