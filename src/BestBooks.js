a694b1bb0f243bfb9fa91eef50c14e442bd4f93e
KC Hofstetter <kclynhofstetter@gmail.com>
2022-06-28 17:49:41 -0700

    Complete lab 11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import { Carousel, Image, Container, Route } from 'react-bootstrap';
+import './BestBooks.css';
+// import About from './About.js';
+
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,23 +14,21 @@ class BestBooks extends React.Component {
     }
   }
 
-componentDidMount(){
-  this.getBooks();
-}
+  componentDidMount() {
+    this.getBooks();
+  }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-  getBooks = async (e) => {
-    e.preventDefault();
-
-    try{
-      let results = await axios.get(`${process.env.REACT_APP_server}/books`)
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`)
       this.setState({
         books: results.data,
       })
       console.log(this.state.books)
     } catch (error) {
       this.setState({
-      bookErrorMessage: `An error occured: ${error.response.status}, ${error.response.data}`
+        bookErrorMessage: `An error occured: ${error.response.status}, ${error.response.data}`
       })
     }
   }
@@ -33,16 +36,30 @@ componentDidMount(){
   render() {
 
     /* TODO: render all the books in a Carousel */
-
+    let carouselItems =
+      this.state.books.length ? this.state.books.map((book, _id) => (
+        <Carousel.Item className="my-carousel-item" key={book._id}>
+          <Image className="d-block item" src={book.img} alt={book.title} />
+          <Carousel.Caption className="caption">
+            <h3>{book.title}</h3>
+            <p>{book.description}</p>
+            <p>Available in stores: {book.status ? 'yes' : 'no'} </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )) : (<h3>No Books Found :</h3>)
     return (
       <>
         <h2>Books From A Favorite Author &amp; Book Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        {/* <Route
+              exact path="/about"
+              element={<About/>}
+            >
+        </Route> */}
+        <div className='my-carousel'>
+          <Carousel>
+            {carouselItems}
+          </Carousel>
+        </div>
       </>
     )
   }
